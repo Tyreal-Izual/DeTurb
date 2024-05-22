adab130946acf42484aecd47e8e68c8d129ec7d3
Tyreal-Izual <zouzhicheng1348099766@outlook.com>
2024-05-22 17:39:04 +0100

    bugfix

--- code/video_inferencefolder2.py
+++ code/video_inferencefolder2.py
@@ -1,12 +1,11 @@
 import cv2
 import torch
-import argparse
-import os
-import time
+import argparse, os, time
 import numpy as np
 import torchvision.transforms.functional as TF
 from PIL import Image
 from model.TMT import TMT_MS
+
 
 def split_to_patches(h, w, s):
     nh = h // s + 1
@@ -29,6 +28,7 @@ def split_to_patches(h, w, s):
         wpos.append(w_start)
     return hpos, wpos
 
+
 def test_spatial_overlap(input_blk, model, patch_size):
     _, c, l, h, w = input_blk.shape
     hpos, wpos = split_to_patches(h, w, patch_size)
@@ -36,11 +36,12 @@ def test_spatial_overlap(input_blk, model, patch_size):
     out_masks = torch.zeros_like(input_blk)
     for hi in hpos:
         for wi in wpos:
-            input_ = input_blk[..., hi:hi+patch_size, wi:wi+patch_size]
+            input_ = input_blk[..., hi:hi + patch_size, wi:wi + patch_size]
             output_ = model(input_)
-            out_spaces[..., hi:hi+patch_size, wi:wi+patch_size].add_(output_)
-            out_masks[..., hi:hi+patch_size, wi:wi+patch_size].add_(torch.ones_like(input_))
+            out_spaces[..., hi:hi + patch_size, wi:wi + patch_size].add_(output_)
+            out_masks[..., hi:hi + patch_size, wi:wi + patch_size].add_(torch.ones_like(input_))
     return out_spaces / out_masks
+
 
 def restore_PIL(tensor, b, fidx):
     img = tensor[b, fidx, ...].data.squeeze().float().cpu().clamp_(0, 1).numpy()
@@ -49,8 +50,9 @@ def restore_PIL(tensor, b, fidx):
     img = (img * 255.0).round().astype(np.uint8)  # float32 to uint8
     return img
 
+
 def get_args():
-    parser = argparse.ArgumentParser(description='Video inference with overlapping patches')
+    parser = argparse.ArgumentParser(description='Batch video inference with overlapping patches')
     parser.add_argument('--patch_size', '-ps', dest='patch_size', type=int, default=256, help='spatial patch size')
     parser.add_argument('--temp_patch', type=int, default=12, help='temporal patch size')
     parser.add_argument('--resize_ratio', type=float, default=1.0, help='spatial resize ratio for both w and h')
@@ -58,7 +60,6 @@ def get_args():
                         help='first frame to be processed, if < 1, it is ratio w.r.t. the entire video, if >1, it is absolute value')
     parser.add_argument('--total_frames', type=int, default=-1, help='number of total frames to be processed')
     parser.add_argument('--input_folder', type=str, default=None, help='path of input folder containing videos')
-    parser.add_argument('--single_video', type=str, default=None, help='path of a single video file to process')
     parser.add_argument('--out_folder', type=str, default=None, help='path of output folder to save processed videos')
     parser.add_argument('--model_path', type=str, default=None, help='Load model from a .pth file')
     parser.add_argument('--save_images', action='store_true', help='save results as images')
@@ -66,45 +67,6 @@ def get_args():
     parser.add_argument('--concatenate_input', action='store_true', help='concatenate input and output frames')
     return parser.parse_args()
 
-def process_video(video_path, output_path, args, net):
-    print(f'processing {video_path}')
-    video = cv2.VideoCapture(video_path)
-
-    h, w = int(video.get(4)), int(video.get(3))
-    fps = int(video.get(5))
-    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    start_frame = int(args.start_frame * total_frames) if args.start_frame < 1 else int(args.start_frame)
-    video.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
-
-    if args.total_frames > 0:
-        total_frames = args.total_frames
-
-    hh, ww = (int(h * args.resize_ratio), int(w * args.resize_ratio)) if args.resize_ratio != 1 else (h, w)
-
-    frames = [cv2.resize(video.read()[1], (ww, hh)) for _ in range(total_frames)]
-    video.release()
-
-    patch_size = args.patch_size
-
-    for _ in range(2):
-        inp_frames = [Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)) for img in frames]
-        inp_frames = [TF.to_tensor(TF.resize(img, (hh, ww))) for img in inp_frames]
-
-        input_ = torch.stack(inp_frames, dim=1).unsqueeze(0).cuda()
-        if max(hh, ww) < patch_size:
-            recovered = net(input_)
-        else:
-            recovered = test_spatial_overlap(input_, net, patch_size)
-
-        recovered = recovered.permute(0, 2, 1, 3, 4)
-        frames = [cv2.cvtColor(restore_PIL(recovered, 0, j), cv2.COLOR_RGB2BGR) for j in range(recovered.shape[2])]
-
-    if args.save_video:
-        output_writer = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, (ww, hh))
-        for frame in frames:
-            output_writer.write(frame)
-        output_writer.release()
 
 if __name__ == '__main__':
     args = get_args()
@@ -122,13 +84,86 @@ if __name__ == '__main__':
     for name, param in net.named_parameters():
         param.requires_grad = False
 
-    if args.single_video:
-        video_file = args.single_video
-        out_path = os.path.join(args.out_folder, os.path.splitext(os.path.basename(video_file))[0] + '.mp4')
-        process_video(video_file, out_path, args, net)
-    else:
-        video_files = [f for f in os.listdir(args.input_folder) if f.endswith(('.mp4', '.avi'))]
-        for video_file in video_files:
-            vpath = os.path.join(args.input_folder, video_file)
-            out_path = os.path.join(args.out_folder, os.path.splitext(video_file)[0] + '.mp4')
-            process_video(vpath, out_path, args, net)
+    video_files = [f for f in os.listdir(args.input_folder) if f.endswith(('.mp4', '.avi'))]
+
+    for video_file in video_files:
+        vpath = os.path.join(args.input_folder, video_file)
+        out_path = os.path.join(args.out_folder, os.path.splitext(video_file)[0] + '.mp4')
+
+        if args.save_images:
+            frame_dir = os.path.join(args.out_folder, os.path.splitext(video_file)[0])
+            os.makedirs(frame_dir, exist_ok=True)
+
+        print(f'processing {vpath}')
+        all_frames = []
+        turb_vid = cv2.VideoCapture(vpath)
+
+        h, w = int(turb_vid.get(4)), int(turb_vid.get(3))
+        fps = int(turb_vid.get(5))
+        total_frames = int(turb_vid.get(cv2.CAP_PROP_FRAME_COUNT))
+        start_frame = int(args.start_frame * total_frames) if args.start_frame < 1 else int(args.start_frame)
+        turb_vid.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+        if args.total_frames > 0:
+            total_frames = args.total_frames
+        test_frame_info = [{'start': 0, 'range': [0, 9]}]
+        num_chunk = (total_frames - 1) // (args.temp_patch // 2)
+        for i in range(1, num_chunk):
+            if i == num_chunk - 1:
+                test_frame_info.append({'start': total_frames - args.temp_patch,
+                                        'range': [i * (args.temp_patch // 2) + (args.temp_patch // 4), total_frames]})
+            else:
+                test_frame_info.append({'start': i * (args.temp_patch // 2),
+                                        'range': [i * (args.temp_patch // 2) + (args.temp_patch // 4),
+                                                  i * (args.temp_patch // 2) + (args.temp_patch // 4 * 3)]})
+
+        all_frames = [turb_vid.read()[1] for i in range(total_frames)]
+        turb_vid.release()
+
+        if args.resize_ratio != 1:
+            hh = int(h * args.resize_ratio)
+            ww = int(w * args.resize_ratio)
+            all_frames = [cv2.resize(f, (hh, ww)) for f in all_frames]
+        else:
+            hh, ww = h, w
+
+        patch_size = args.patch_size
+        inp_frames = []
+        out_frames = []
+        frame_idx = 0
+
+        start_t = time.time()
+        for i in range(num_chunk):
+            print(f'{i}/{num_chunk}')
+            out_range = test_frame_info[i]['range']
+            in_range = [test_frame_info[i]['start'], test_frame_info[i]['start'] + args.temp_patch]
+
+            for pass_num in range(2):
+                inp_imgs = [Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)) for img in all_frames[in_range[0]:in_range[1]]]
+                inp_imgs = [TF.to_tensor(TF.resize(img, (hh, ww))) for img in inp_imgs]
+                input_ = torch.stack(inp_imgs, dim=1).unsqueeze(0).cuda()
+                if max(hh, ww) < patch_size:
+                    recovered = net(input_)
+                else:
+                    recovered = test_spatial_overlap(input_, net, patch_size)
+                recovered = recovered.permute(0, 2, 1, 3, 4)
+                input_ = input_.permute(0, 2, 1, 3, 4)
+                for j in range(out_range[0] - in_range[0], out_range[1] - in_range[0]):
+                    out = cv2.cvtColor(restore_PIL(recovered, 0, j), cv2.COLOR_RGB2BGR)
+                    all_frames[in_range[0] + j] = out
+
+            for j in range(out_range[0] - in_range[0], out_range[1] - in_range[0]):
+                final_output = all_frames[in_range[0] + j]
+                out_frames.append(final_output)
+
+        print(f'total time consumption is {time.time() - start_t} s')
+
+        if args.save_video:
+            if args.concatenate_input:
+                output_writer = cv2.VideoWriter(out_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, (ww * 2, hh))
+                for fid, frame in enumerate(out_frames):
+                    output_writer.write(np.concatenate((inp_frames[fid], frame), axis=1))
+            else:
+                output_writer = cv2.VideoWriter(out_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, (ww, hh))
+                for fid, frame in enumerate(out_frames):
+                    output_writer.write(frame)
+            output_writer.release()
